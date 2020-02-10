a9e8d297325dd48348cb41accec4af5df7acc8f6
Samuel Platon <samuel.platon@etudiant.univ-lr.fr>
2020-02-10 17:41:22 +0100

    Ajout du début de la partie 'Configuration du build' du TP

--- base_tp2/api/app.py
+++ base_tp2/api/app.py
@@ -4,8 +4,14 @@ import json
 app = Flask(__name__)
 
 # Route et fonction plus_one
+@app.route('/plus_one/<int:x>')
+def plus_one(x):
+    return str(x+1)
 
 # Route et fonction square 
+@app.route('/square/<int:x>')
+def square(x):
+    return str(x*x)
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=int("5000"), debug=True)
--- base_tp2/tests/test_api.py
+++ base_tp2/tests/test_api.py
@@ -1,5 +1,6 @@
 from urllib.parse import urlencode
 import json
+import pytest
 
 def call(client, path, params):
   url = path + '?' + urlencode(params)
@@ -7,5 +8,13 @@ def call(client, path, params):
   return json.loads(response.data.decode('utf-8'))
 
 # Test fonction plus_one
+def test_plus_one(client):
+    """Start with a blank database."""
+    result = call(client, '/plus_one/5', {})
+    assert result == 6
 
 # Test fonction square
+def test_square(client):
+    """Start with a blank database."""
+    result = call(client, '/square/5', {})
+    assert result == 25
