701684385dfcdcccd01e401ff7fe1fad98496b9c
Samuel Platon <samuel.platon@etudiant.univ-lr.fr>
2020-02-14 14:31:20 +0100

    Mise à jour de l'API et des Requirements selon la première partie du TP3

--- api/app.py
+++ api/app.py
@@ -1,17 +1,29 @@
 from flask import request, Flask
+from flask_restplus import Api, Resource, fields
 import json
 
 app = Flask(__name__)
+api = Api(app, version='1.0', title='ECI ADM/Devops demo API',
+  description='Demo API',
+)
+
 
 # Route et fonction plus_one
 @app.route('/plus_one/<int:x>')
-def plus_one(x):
-    return str(x+1)
+@api.doc(params={'x': 'An Integer'})
+class PlusOne(Resource):
+  def get(self, x):
+    x = int(x)
+    return {"x": x + 1}
+
 
 # Route et fonction square 
-@app.route('/square/<int:x>')
-def square(x):
-    return str(x*x)
+@api.route('/square')
+@api.doc(params={'x': 'An Integer'}, location='query')
+class Square(Resource):
+  def get(self):
+    x = int(request.args.get('x', 1))
+    return {"x": x * x}
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=int("5000"), debug=True)
