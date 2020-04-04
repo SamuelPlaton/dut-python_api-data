a14d68da117dfd2d4a141d6e076b3065aca8567c
Samuel Platon <samuel.platon@etudiant.univ-lr.fr>
2020-04-04 18:57:15 +0200

    Deleted useless tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -6,15 +6,3 @@ def call(client, path, params):
   url = path + '?' + urlencode(params)
   response = client.get(url)
   return json.loads(response.data.decode('utf-8'))
-
-# Test fonction plus_one
-def test_plus_one(client):
-    """Start with a blank database."""
-    result = call(client, '/plus_one/5', {})
-    assert result == {"x": 6}
-
-# Test fonction square
-def test_square(client):
-    """Start with a blank database."""
-    result = call(client, '/square/5', {})
-    assert result == {"x": 25}
