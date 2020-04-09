d6604001b423cba3517c25b55c121f3c3a8ea6b7
Samuel Platon <samuel.platon@etudiant.univ-lr.fr>
2020-04-09 09:18:19 +0200

    Added the first test about timeSpeech

--- tests/test_api.py
+++ tests/test_api.py
@@ -6,3 +6,19 @@ def call(client, path, params):
   url = path + '?' + urlencode(params)
   response = client.get(url)
   return json.loads(response.data.decode('utf-8'))
+
+def test_timeSpeech(client):
+	result = call(client, '/timeSpeech', {})
+	print(result)
+	assert result == {
+    "Cartman": "20.19%",
+    "Kyle": "7.3%",
+    "Stan": "5.99%",
+    "Randy": "4.01%",
+    "Mrs. Garrison": "2.55%",
+    "Butters": "2.28%",
+    "Mr. Mackey": "2.14%",
+    "Al Gore": "1.77%",
+    "Liane": "1.73%",
+    "Mr. Connolly": "1.64%"
+}
